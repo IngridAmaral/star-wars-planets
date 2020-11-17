6ad2023e43d5e473f6725df1a218cfa78b2fef4e
Ingrid Amaral <ingridamaralrj@gmail.com>
2020-11-17 16:20:07 +0100

    connects to the store and fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { connect, ConnectedProps } from 'react-redux';
+import { bindActionCreators, Action, Dispatch } from 'redux';
+import fetchPlanetsDispatcher from './redux/actions-dispatcher/planets';
+import { RootState } from './redux/store';
+import { getPlanets } from './redux/reducers/planets';
 import './App.scss';
 
-const App = (): JSX.Element => {
-  return (
-    <div className="App">
-      <header className="App-header">hello</header>
-    </div>
-  );
-};
+const mapStateToProps = (state: RootState) => ({
+  planets: getPlanets(state)
+});
 
-export default App;
+const mapDispatchToProps = (dispatch: Dispatch<Action>) =>
+  bindActionCreators({ fetchPlanets: fetchPlanetsDispatcher }, dispatch);
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type AppProps = PropsFromRedux;
+class App extends React.Component<AppProps> {
+  componentDidMount() {
+    const { fetchPlanets } = this.props;
+    fetchPlanets();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">hello</header>
+      </div>
+    );
+  }
+}
+
+export default connector(App);
